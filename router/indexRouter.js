ff2c325391f502b38d2e91d804c610efaa11d24f
Bread-John <mr.admin@johnnybread.com>
2022-07-19 20:42:45 +1000

    code refactored

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,31 +1,19 @@
 const express = require('express');
-const fs = require('fs');
 const nodeID3 = require('node-id3');
 const path = require('path');
 
-const fileUpload = require('../lib/fileUploads');
+const multer = require('../lib/multer');
 const { convertToHlsLossy } = require('../lib/ffmpeg');
-const { parseRange, validateRange, extToMIME} = require('../lib/variableStreamUtil');
+const { parseRange, validateRange, extToMIME } = require('../lib/varStreamUtil');
 const { UserFacingError } = require('../lib/customError');
 const { newTransaction } = require('../dao/main');
 const { Artist, Album, Song } = require('../dao/config').models;
-const { getUserDetails } = require('../lib/msgraph/User');
-const { getFileInfoByName, getFileByName, getPartialFileByName, createFolder, uploadFile } = require('../lib/msgraph/File');
+const { getItemByPath, getFileByPath, getFileSliceByPath, uploadFileToParent } = require('../lib/msgraph/File');
 
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
     res.status(200).send('<h2>Hello, world!</h2>');
-});
-
-router.get('/user', function (req, res, next) {
-    getUserDetails(req.app.locals.msalClient, req.session.userId.split('.')[0])
-        .then(function (response) {
-            res.status(200).json(response);
-        })
-        .catch(function (error) {
-            next(error);
-        });
 });
 
 router.get('/play', async function (req, res, next) {
@@ -47,22 +35,25 @@ router.get('/play', async function (req, res, next) {
     }
 });
 
-router.get('/stream/:resourceId/:resourceName', async function (req, res, next) {
-    const { resourceId, resourceName } = req.params;
+router.get('/stream/:resourceName', async function (req, res, next) {
+    const { resourceName } = req.params;
+    const [, resourceId] = resourceName.split('_');
     try {
         const song = await Song.findOne({ where: { fileIdentifier: resourceId } });
         if (!song) {
-            next(new UserFacingError(`Resource ${resourceId} does not exist`, 404));
+            next(new UserFacingError(`Resource does not exist`, 404));
         } else {
+            const remotePath = `stream/${resourceName}`;
+
             const range = req.headers['range'];
             if (range) {
-                getFileInfoByName(req.app.locals.msalClient, resourceName)
+                getItemByPath(req.app.locals.msalClient, remotePath)
                     .then(function (info) {
                         const [start, end] = parseRange(range, info.size);
                         if (!validateRange(start, end, info.size)) {
                             next(new UserFacingError(`Requested range is not inside the size of the resource`, 416));
                         } else {
-                            getPartialFileByName(req.app.locals.msalClient, resourceName, start, end)
+                            getFileSliceByPath(req.app.locals.msalClient, remotePath, start, end)
                                 .then(function (stream) {
                                     const resHeaders = {
                                         'Accept-Ranges': `bytes`,
@@ -80,8 +71,11 @@ router.get('/stream/:resourceId/:resourceName', async function (req, res, next) 
                                 });
                         }
                     })
+                    .catch(function (error) {
+                        next(error);
+                    });
             } else {
-                getFileByName(req.app.locals.msalClient, resourceName)
+                getFileByPath(req.app.locals.msalClient, remotePath)
                     .then(function (stream) {
                         const resHeaders = { 'Content-Type': extToMIME(path.extname(resourceName)) };
                         res.writeHead(200, resHeaders);
@@ -99,24 +93,48 @@ router.get('/stream/:resourceId/:resourceName', async function (req, res, next) 
     }
 });
 
-router.post('/upload', fileUpload.single('media'), async function (req, res, next) {
+router.post('/upload', multer.single('media'), async function (req, res, next) {
     if (!req.file) {
         next(new UserFacingError(`Uploaded file is not accepted`, 400));
     } else {
-        const fileIdentifier = req.file.filename.split('_').at(1);
+        const fileIdentifier = path.basename(req.file.path, path.extname(req.file.path));
 
         // Details on these tag names are here: https://github.com/Zazama/node-id3#supported-raw-ids
         const {
             TIT2, TPE1, TALB, TPE2, TCON,
-            TRCK, TPOS, TCOM, TYER, TPUB,
-            APIC
+            TRCK, TPOS, TCOM, TYER, TPUB
         } = nodeID3.read(req.file.path, { onlyRaw: true });
-        const imageBuffer = APIC ? APIC.imageBuffer : undefined;
         const [trackNo, totalTrackNo] = TRCK ? TRCK.split('/') : [];
         const [discNo, totalDiscNo] = TPOS ? TPOS.split('/') : [];
 
         const t = await newTransaction();
         try {
+            const outputFiles = await convertToHlsLossy(fileIdentifier, req.file.filename);
+            for (const file of outputFiles) {
+                const { id: folderId } = await getItemByPath(req.app.locals.msalClient, 'stream');
+                const result = await uploadFileToParent(req.app.locals.msalClient, file, folderId);
+                console.log(
+                    `[${new Date(Date.now()).toUTCString()}] - MSGraph Info: File "${result['name']}" has been uploaded`
+                );
+                /*fs.rm(file, function (err) {
+                    if (err) throw err;
+                    console.log(
+                        `[${new Date(Date.now()).toUTCString()}] - FileSys Info: File at path "${file}" has been deleted`
+                    );
+                });*/
+            }
+            const { id: folderId } = await getItemByPath(req.app.locals.msalClient, 'source');
+            const result = await uploadFileToParent(req.app.locals.msalClient, req.file.path, folderId);
+            console.log(
+                `[${new Date(Date.now()).toUTCString()}] - MSGraph Info: File "${result['name']}" has been uploaded`
+            );
+            /*fs.rm(req.file.path, function (err) {
+                if (err) throw err;
+                console.log(
+                    `[${new Date(Date.now()).toUTCString()}] - FileSys Info: File at path "${req.file.path}" has been deleted`
+                );
+            });*/
+
             const [artist, ] = await Artist.findOrCreate({
                 where: { name: TPE1 || 'Unnamed Artist' },
                 transaction: t
@@ -168,42 +186,21 @@ router.post('/upload', fileUpload.single('media'), async function (req, res, nex
                 transaction: t
             });
 
-            await convertToHlsLossy(fileIdentifier, req.file.filename);
-
-            const folder = await createFolder(req.app.locals.msalClient, fileIdentifier);
-
-            fs.readdir(req.file.destination, async function (err, files) {
-                if (err) throw err;
-                for (const file of files) {
-                    const result = await uploadFile(req.app.locals.msalClient, `${req.file.destination}/${file}`, file, folder);
-                    if (result) {
-                        console.log(
-                            `[${new Date(Date.now()).toUTCString()}] - OneDrive Info: File "${file}" has been uploaded`
-                        );
-                        fs.rm(`${req.file.destination}/${file}`, function (err) {
-                            if (err) throw err;
-                            console.log(
-                                `[${new Date(Date.now()).toUTCString()}] - FileSys Info: Local copy of File "${file}" has been deleted`
-                            );
-                        });
-                    }
-                }
-            });
-
             await t.commit();
 
             res.status(200).json({
-                message: `Music ( ${artist.name} - ${song.title} ) has been uploaded`
+                ack: true,
+                message: `Music (${artist.name} - ${song.title}) has been uploaded`
             });
         } catch (error) {
             await t.rollback();
 
-            fs.rm(req.file.destination, { recursive: true, force: true }, function () {
+            /*fs.rm(req.file.path, function () {
                 console.log(
-                    `[${new Date(Date.now()).toUTCString()}] - FileSys Info: Temp Folder "${req.file.destination}" has been cleaned up`
+                    `[${new Date(Date.now()).toUTCString()}] - FileSys Info: File at path "${req.file.path}" has been deleted`
                 );
                 next(error);
-            });
+            });*/
         }
     }
 });
