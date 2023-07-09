150271ec1ed1643bd77f5e043770bf99b2e622b1
Bread-John <mr.admin@johnnybread.com>
2023-07-09 19:45:58 +1000

    db initialisation process updated

--- dao/main.js
+++ dao/main.js
@@ -14,12 +14,9 @@ module.exports = {
     },
     syncModels: function (alterSync) {
         return new Promise(function (resolve, reject) {
-            sequelize.sync({ alter: alterSync })
-                .then(async function () {
-                    const [results, ] = await sequelize.query(`SELECT * FROM pg_extension WHERE extname='pg_trgm';`)
-                    if (!results) {
-                        await sequelize.query(`CREATE EXTENSION pg_trgm;`);
-                    }
+            sequelize
+                .sync({ alter: alterSync })
+                .then(function () {
                     resolve();
                 })
                 .catch(function (err) {
